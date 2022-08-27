14f902f5094c16435c6f2c55002868cee95eb4ce
Sami <78439283+mhmdsami@users.noreply.github.com>
2022-08-26 20:48:34 -0700

    add: third largest number

---
+++ Searching/third-largest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void third_largest(int *arr,int arr_size)
+{
+    int i, j;
+    for (i = 0; i < arr_size; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    for (i = 0; i < arr_size; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+}
+
+int main()
+{
+    int arr_size, *arr, i;
+    scanf("%d", &arr_size);
+    arr = (int *)malloc(sizeof(int) * arr_size);
+    for (i = 0; i < arr_size; i++)
+    {
+        scanf("%d", arr + i);
+    }
+    third_largest(arr, arr_size);
+    return 0;
+}
