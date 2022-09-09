ee3b4034489f7af54ef6ddcb1d7cc8ae0a09710c
Sami <78439283+mhmdsami@users.noreply.github.com>
2022-09-08 20:56:02 -0700

    add: sorting: minimum extral petrol

---
+++ Sorting/minimum-extra-petrol.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void sort(int *arr, int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i; j < n; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n, k, i, *arr;
+        scanf("%d %d", &n, &k);
+        arr = (int *)malloc(sizeof(int) * n);
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        sort(arr, n);
+        printf("\n");
+        printf("%d\n", arr[0] - k > 0 ? arr[0] - k : -1);
+    }
+    return 0;
+}
